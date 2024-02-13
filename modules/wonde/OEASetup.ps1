b1a642a19817962b375cc9fcb3442a2f779a7637
Matt Woodruff <100275472+themattwoodruffgit@users.noreply.github.com>
2024-02-13 10:58:41 +0530

    Update OEASetup.ps1

--- modules/wonde/OEASetup.ps1
+++ modules/wonde/OEASetup.ps1
@@ -1,156 +1,126 @@
+function Upload-NotebookFiles {
+    param (
+        [System.IO.FileInfo[]]$NotebookFiles,
+        [System.Collections.ArrayList]$ExistingItems,
+        [string]$FabricCreateUrl,
+        [System.Collections.Hashtable]$Headers
+    )
+
+    foreach ($notebookFile in $NotebookFiles) {
+        $notebookContent = Get-Content -Path $notebookFile.FullName -Raw
+        $base64Content = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($notebookContent))
+        Write-Output "Notebook $($notebookFile.Name) converted to inline Base64 string:"
+        
+        $notebookName = $notebookFile.BaseName
+        Write-Output $notebookName
+        $notebookInstance = $ExistingItems | Where-Object { $_.DisplayName -eq $notebookName }
+        
+        if ($notebookInstance) {
+            Write-Host "Notebook $notebookName is already present $($notebookInstance)"
+        }
+        else {
+            $jsonBody = @{
+                displayName = $notebookName
+                type = "Notebook"
+                definition = @{
+                    format = "ipynb"
+                    parts = @(
+                        @{
+                            path = "artifact.content.ipynb"
+                            payload = $base64Content
+                            payloadType = "InlineBase64"
+                        }
+                    )
+                }
+            } | ConvertTo-Json
+
+            $response = Invoke-RestMethod -Uri $FabricCreateUrl -Method Post -Body $jsonBody -Headers $Headers
+            Write-Host "$($notebookFile.Name) is created"
+        }
+    }
+}
+
 function Configure-OEAI {
     param (
         [string]$WorkspaceId,
         [string]$CapacityName,
-	[string]$subscriptionName,
-	[string]$resourceGroupName,
-	[string]$powerbiEmbeddedEntraId
+        [string]$subscriptionName,
+        [string]$resourceGroupName,
+        [string]$powerbiEmbeddedEntraId
     )
-	
-	Write-Host "Please check if you are administrator of fabric capacity, go to capacity in azure and click on Capacity Administrators"
-	
-	# Check if Az.Accounts module is installed
+
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+    Write-Host "Please check if you are administrator of fabric capacity, go to capacity in azure and click on Capacity Administrators"
+
     $azAccountsModule = Get-Module -Name Az.Accounts -ListAvailable
 
     if (-not $azAccountsModule) {
-		Install-Module Az.Accounts -Scope CurrentUser
-	}
-	
-	# Check if Fabtools module is installed
+        Install-Module Az.Accounts -Scope CurrentUser
+    }
+
     $fabtoolsModule = Get-Module -Name Fabtools -ListAvailable
 
     if (-not $fabtoolsModule) {
-		Install-Module Fabtools -Scope CurrentUser
-	}
-	
+        Install-Module Fabtools -Scope CurrentUser
+    }
 
-#Create key vault
-az account set --subscription $subscriptionName
+    az account set --subscription $subscriptionName
 
-az keyvault create --name 'kv-oea-oeai' --resource-group $resourceGroupName --location "uksouth"
+    az keyvault create --name 'kv-oea-oeai' --resource-group $resourceGroupName --location "uksouth"
 
-# Get the Application (client) ID of the Power BI App
-appId=$powerbiEmbeddedEntraId
-# Set Key Vault access policy for secrets
-az keyvault set-policy --name 'kv-oea-oeai' --resource-group $resourceGroupName --object-id $appId --secret-permissions get list
+    $appId = $powerbiEmbeddedEntraId
 
-# Clone the repository
-git clone https://github.com/Jojobit/fabtools.git
+    az keyvault set-policy --name 'kv-oea-oeai' --resource-group $resourceGroupName --object-id $appId --secret-permissions get list
 
-# Import the module
-Import-Module ./Fabtools/Fabtools.psm1
+    git clone https://github.com/Jojobit/fabtools.git
 
-Set-FabricAuthToken -reset
-#Connect-AzAccount
-$accessToken = Get-FabricAuthToken
- $accessToken = "Bearer ${accessToken}"
-$headers = @{
-    "Content-Type" = "application/json"
-    "Authorization" = $accessToken
-}
+    Import-Module ./Fabtools/Fabtools.psm1
 
+    Set-FabricAuthToken -reset
 
-$response = Invoke-RestMethod -Uri "https://api.fabric.microsoft.com/v1/capacities" -Method GET -Headers $headers
+    $accessToken = Get-FabricAuthToken
+    $accessToken = "Bearer ${accessToken}"
+    $headers = @{
+        "Content-Type" = "application/json"
+        "Authorization" = $accessToken
+    }
 
+    $response = Invoke-RestMethod -Uri "https://api.fabric.microsoft.com/v1/capacities" -Method GET -Headers $headers
 
-$response.value | Format-Table
+    $response.value | Format-Table
 
-$capacity = $response.value | Where-Object { $_.displayName -eq $CapacityName }
+    $capacity = $response.value | Where-Object { $_.displayName -eq $CapacityName }
 
+    Register-FabricWorkspaceToCapacity -WorkspaceId $WorkspaceId -CapacityId $capacity.Id
 
-Register-FabricWorkspaceToCapacity -WorkspaceId $WorkspaceId -CapacityId $capacity.Id
+    $fabricCreateUrl = "https://api.fabric.microsoft.com/v1/workspaces/$($WorkspaceId)/items"
+    $listResponse = Invoke-RestMethod -Uri $fabricCreateUrl -Method Get -Headers $headers
+    $existingItems = $listResponse.Value
 
-$fabricCreateUrl = "https://api.fabric.microsoft.com/v1/workspaces/$($WorkspaceId)/items"
-#list existing items# Make the API request
-$listResponse = Invoke-RestMethod -Uri $fabricCreateUrl -Method Get -Headers $headers
-$existingItems = {}
+    Write-Host $existingItems | Format-Table -AutoSize
 
-# Display data in a table
-$existingItems = $listResponse.Value
+    $lakeHouseInstance = $existingItems | Where-Object { $_.DisplayName -eq 'oealkhouse' }
+    Write-Host "value of $($lakeHouseInstance)"
+    if ($lakeHouseInstance) {
+        Write-Host "oealkhouse is already present $($lakeHouseInstance)"
+    }
+    else {
+        $body = @{
+            displayName = "oealkhouse"
+            type = "Lakehouse"
+        } | ConvertTo-Json
 
+        $response = Invoke-RestMethod -Uri $fabricCreateUrl -Method Post -Body $body -Headers $headers
+        write-Host "Lakehouse is created"
+    }
 
+    $notebooksFolder = "."
+    $notebookFiles = Get-ChildItem -Path $notebooksFolder -Filter *.ipynb
+    Upload-NotebookFiles -NotebookFiles $notebookFiles -ExistingItems $existingItems -FabricCreateUrl $fabricCreateUrl -Headers $headers
 
-Write-Host $existingItems| Format-Table -AutoSize
-
-#create lakehouse
-
-$lakeHouseInstance = $existingItems | Where-Object { $_.DisplayName -eq 'oealkhouse' }
-Write-Host "value of $($lakeHouseInstance)"
-if($lakeHouseInstance) { Write-Host "oealkhouse is already present $($lakeHouseInstance)" }
-else{
-	$body = @{
-	  "displayName"= "oealkhouse"
-	  "type"= "Lakehouse"
-	}| ConvertTo-Json
-
-	$response = Invoke-RestMethod -Uri $fabricCreateUrl -Method Post -Body $body -Headers $headers
-	write-Host "Lakehouse is created"
-}
-#upload notebook files
-
-# Set the path to the folder containing IPython Notebook files
-$notebooksFolder = "."
-
-# Get all .ipynb files in the folder
-$notebookFiles = Get-ChildItem -Path $notebooksFolder -Filter *.ipynb
-
-# Loop through each notebook file
-foreach ($notebookFile in $notebookFiles) {
-    # Read the content of the notebook file
-    $notebookContent = Get-Content -Path $notebookFile.FullName -Raw
-
-    # Convert the content to Base64
-    $base64Content = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($notebookContent))
-
-
-    # Display the inline Base64 string or perform any other actions as needed
-    Write-Output "Notebook $($notebookFile.Name) converted to inline Base64 string:"
-    
-	$notebookName = $notebookFile.Name
-
-	# Find the position of the first dot
-	$dotPosition = $notebookName.IndexOf('.')
-
-	# Check if a dot was found
-	if ($dotPosition -ge 0) {
-		# Trim the string up to the first dot
-		$notebookName = $notebookName.Substring(0, $dotPosition)
-		
-		# Display the trimmed string
-		Write-Output "Trimmed String: $trimmedString"
-	}
-	
-	Write-Output $notebookName
-	$notebookInstance = $existingItems | Where-Object { $_.DisplayName -eq $notebookName }
-	
-	if($notebookInstance){Write-Host "oealkhouse is already present $($notebookInstance)"}
-	else{
-		# Construct the JSON string using double-quoted string to expand $name
-		$jsonBody = @"
-		{
-			"displayName": "$notebookName",
-			"type": "Notebook",
-			"definition": {
-				"format": "ipynb",
-				"parts": [
-					{
-						"path": "artifact.content.ipynb",
-						"payload": "$base64Content",
-						"payloadType": "InlineBase64"
-					}
-				]
-			}
-		}
-"@
-
-
-		$response = Invoke-RestMethod -Uri $fabricCreateUrl -Method Post -Body $jsonBody -Headers $headers
-
-		Write-Host "$($notebookFile.Name) is created"
-	}
-
-}
-
-
+    #for oeai_py in root folder
+    $notebooksFolder = "..\..\."
+    $notebookFiles = Get-ChildItem -Path $notebooksFolder -Filter *.ipynb
+    Upload-NotebookFiles -NotebookFiles $notebookFiles -ExistingItems $existingItems -FabricCreateUrl $fabricCreateUrl -Headers $headers
 
 }
